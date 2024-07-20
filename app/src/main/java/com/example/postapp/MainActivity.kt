4a085e584056043144bbe2679bf3b26b3049c51b
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-07-20 11:58:02 +0300

    Ignored files

---
+++ app/src/main/java/com/example/postapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.postapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.postapp.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+     lateinit var binding: ActivityMainBinding
+     lateinit var recyclerView: RecyclerView
+     lateinit var postAdapter: PostAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater) // Initialize the binding
+        setContentView(binding.root)
+        fetchPosts()
+        recyclerView = binding.rvApi
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+    fun fetchPosts() {
+        val apiInterface = ApiClient.buildApiInterface(PostsApiInterface::class.java)
+        val request = apiInterface.fetchPosts()
+        request.enqueue(object : Callback<List<Post>> {
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                if (response.isSuccessful) {
+                    val posts = response.body()
+                    posts?.let {
+                        postAdapter = PostAdapter(it)
+                        recyclerView.adapter = postAdapter
+                    }
+                    Toast.makeText(baseContext, "Fetched ${posts!!.size} posts", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(baseContext, response.errorBody()?.string(), Toast.LENGTH_LONG).show()
+                }
+            }
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
+
+
+
+
+
